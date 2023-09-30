a211f4b3c9759c0ea72fc1feabe41eacce3c27bf
TTGioi <tiengioitran@gmail.com>
2023-10-01 05:04:30 +0700

    push 1/10

--- controllers/web/admin.web.controller.js
+++ controllers/web/admin.web.controller.js
@@ -1,8 +1,55 @@
 let adminModel = require('../../models/admin.model').AdminModel;
 
 exports.list = async (req, res, next) => {
-    let arr_admin = await adminModel.find();
-    res.send(arr_admin)
+    const messages = await req.consumeFlash('info');
+    const locals = {
+      title: 'NodeJs',
+      description: 'Free NodeJs User Management System'
+    }
+
+    let perPage = 2;
+    let page = req.query.page || 1;
+
+    try {
+      const clients = await adminModel.aggregate([ { $sort: { createdAt: -1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec(); 
+      const count = await adminModel.count();
+      console.log("cus: ",clients);
+
+      res.render('viewAdmin', {
+        locals,
+        clients,
+        current: page,
+        pages: Math.ceil(count / perPage),
+        messages
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+exports.view = async (req, res) => {
+
+  try {
+    const customer = await adminModel.findOne({ _id: req.params.id })
+
+    const locals = {
+      title: "View Customer Data",
+      description: "Free NodeJs User Management System",
+    };
+
+    res.render('admin/detailAdm', {
+      locals,
+      customer
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+
 }
 
 exports.register = async (req, res, next) => {
--- controllers/web/product.web.controller.js
+++ controllers/web/product.web.controller.js
@@ -1,8 +1,55 @@
 let productModel = require('../../models/product.model').ProductModel;
 
 exports.list = async (req, res, next) => {
-    let arr_product = await productModel.find().populate('id_discount');
-    res.send(arr_product)
+    const messages = await req.consumeFlash('info');
+    const locals = {
+      title: 'NodeJs',
+      description: 'Free NodeJs User Management System'
+    }
+
+    let perPage = 2;
+    let page = req.query.page || 1;
+
+    try {
+      const clients = await productModel.aggregate([ { $sort: { createdAt: -1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec(); 
+      const count = await productModel.count();
+      console.log("cus: ",clients);
+
+      res.render('viewProduct', {
+        locals,
+        clients,
+        current: page,
+        pages: Math.ceil(count / perPage),
+        messages
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+exports.view = async (req, res) => {
+
+  try {
+    const customer = await productModel.findOne({ _id: req.params.id })
+
+    const locals = {
+      title: "View Customer Data",
+      description: "Free NodeJs User Management System",
+    };
+
+    res.render('product/detailPro', {
+      locals,
+      customer
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+
 }
 
 exports.insert = async (req, res, next) => {
