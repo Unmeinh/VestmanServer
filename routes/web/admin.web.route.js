0e9c0c4d63dde73909832d0dc9c5f88a56f97959
TTGioi <tiengioitran@gmail.com>
2023-10-11 23:29:02 +0700

    push sort

--- routes/web/admin.web.route.js
+++ routes/web/admin.web.route.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var multer = require('multer');
 var uploader = multer({dest:'/public/tmp'});
 
-<<<<<<< HEAD
+
 router.get('/', controller.list);
 router.get('/sort', controller.listSort);
 router.get('/high', controller.listHigh);
@@ -14,13 +14,6 @@ router.post('/register', uploader.any(), controller.register);
 router.get('/edit/:id', controller.edit);
 router.post('/edit', uploader.any(), controller.editPost);
 router.post('/delete/:id', controller.delete);
-=======
-router.get('/', controller.home);
-router.get('/home', controller.home);
-router.get('/admin/view/:id', controller.view);
-router.get('/register', controller.register);
-router.post('/register', controller.register);
->>>>>>> main
 router.get('/admin/dashboard', controller.dashboard);
 
 module.exports = router;
