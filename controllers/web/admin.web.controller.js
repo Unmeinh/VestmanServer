a9c991177841af407734deb2c8db6b837e7f31b8
TTGioi <tiengioitran@gmail.com>
2023-10-11 23:19:59 +0700

    push sort

--- controllers/web/admin.web.controller.js
+++ controllers/web/admin.web.controller.js
@@ -1,10 +1,7 @@
 let adminModel = require('../../models/admin.model').AdminModel;
 let productModel = require('../../models/product.model').ProductModel;
 let billModel = require('../../models/bill.model').BillModel;
-<<<<<<< HEAD
 const { onUploadImages } = require("../../function/uploadImage");
-=======
->>>>>>> main
 let clientModel = require('../../models/client.model').ClientModel;
 
 exports.home = async (req, res, next) => {
@@ -147,7 +144,6 @@ exports.register = async (req, res, next) => {
   
 }
 
-<<<<<<< HEAD
 exports.edit = async (req, res, next) => {
   try {
     const adm = await adminModel.findById({_id : req.params.id});
@@ -286,103 +282,5 @@ async function getTotalCustomer(previusDate, nowDate) {
   }
 }
 
-=======
-exports.dashboard = async (req, res, next) => {
-  try {
-    let listProduct = await productModel.find().sort({ quantitySold: -1 }).limit(5);
-    const months = [];
-    const totalBills = [];
-    const totalProducts = [];
-    const totalInterests = [];
-    const totalCustomers = [];
-    const endDate = new Date();
-    const last6Month = new Date(endDate.getFullYear(), endDate.getMonth() - 5, 1);
-    let currentDate = new Date(last6Month);
 
-    while (currentDate <= endDate) {
-      let previusDate = currentDate.toISOString();
-      let nowDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1).toISOString();
-      let date = currentDate.toLocaleString('en', { month: 'long', year: 'numeric' });
-      months.push(date.substring(0, 1).toLocaleUpperCase() + date.substring(1));
-      currentDate.setMonth(currentDate.getMonth() + 1);
 
-      let total = await getTotalBill(previusDate, nowDate);
-      let product = await getTotalProduct(previusDate, nowDate);
-      let client = await getTotalCustomer(previusDate, nowDate);
-      totalBills.push(total);
-      totalProducts.push(product);
-      totalInterests.push(total - product);
-      totalCustomers.push(client);
-    }
-    res.render('admin/dashboard', {
-      title: 'Dashboard',
-      listProduct: JSON.stringify(listProduct),
-      months: JSON.stringify(months),
-      totalBills: JSON.stringify(totalBills),
-      totalProducts: JSON.stringify(totalProducts),
-      totalInterests: JSON.stringify(totalInterests),
-      totalCustomers: JSON.stringify(totalCustomers),
-    })
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-}
-
-async function getTotalBill(previusDate, nowDate) {
-  var match_stage = {
-    $match: {
-      'created_at': {
-        '$gte': new Date(previusDate),
-        '$lte': new Date(nowDate)
-      }
-    }
-  }
-  var group_stage = {
-    $group: { _id: null, sum: { $sum: "$total" } }
-  }
-  var project_stage = {
-    $project: { _id: 0, total: '$sum' }
-  }
-
-  var pipeline = [match_stage, group_stage, project_stage]
-  let sumTotal = await billModel.aggregate(pipeline);
-  if (sumTotal[0] != undefined) {
-    return sumTotal[0].total;
-  } else {
-    return 0;
-  }
-}
-
-async function getTotalProduct(previusDate, nowDate) {
-  let productPrice = await productModel.find(
-    {
-      created_at: {
-        $gte: previusDate,
-        $lte: nowDate
-      }
-    }
-  );
-  if (productPrice[0] != undefined) {
-    return productPrice[0].price * productPrice[0].quantity;
-  } else {
-    return 0;
-  }
-}
-
-async function getTotalCustomer(previusDate, nowDate) {
-  let listClient = await clientModel.find(
-    {
-      created_at: {
-        $gte: previusDate,
-        $lte: nowDate
-      }
-    }
-  );
-  if (listClient) {
-    return listClient.length;
-  } else {
-    return 0;
-  }
-}
->>>>>>> main
