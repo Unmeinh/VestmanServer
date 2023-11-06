d660d2e8309d862c8764dd72b113bbc519e54f31
Unmeinh <khongphaihoanglinh@gmail.com>
2023-11-06 17:45:29 +0700

    Push bill.api

--- models/bill.model.js
+++ models/bill.model.js
@@ -5,11 +5,14 @@ let BillSchema = new db.mongoose.Schema(
         arr_product: [
             {
                 id_product: { type: db.mongoose.Schema.Types.ObjectId, ref: 'ProductModel' },
+                price: { type: Number, required: true },
                 size: { type: String, required: true },
                 quantity: { type: Number, required: true },
             }
         ],
         total: { type: Number, required: true },
+        customerInfo: { type: Object, required: true },
+        paymentMethod: { type: Number, required: true },
         status: { type: Number, required: true },
         created_at: { type: Date, required: true },
     },
